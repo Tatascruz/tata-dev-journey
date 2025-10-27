724b5cd06f2bb84ff691ce3a6e8a3039dbf58377
Thalita <thalitascruz@icloud.com>
2025-10-27 14:52:29 -0300

    Adiciona produto na linguagem Go

---
+++ produto.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	nomeProduto := "Caixa Presenteavel"
+	material := "Linho"
+	valor := 65.00
+
+	fmt.Printf("Produto: %s\nMaterial: %s\nValor: R$ %.2f\n", nomeProduto, material, valor)
+}
