e6a975c8bb1416f425761110fdbde34b2cc4436d
Thalita <thalitascruz@icloud.com>
2025-12-09 20:13:46 -0300

    feat(api): adiciona rota /status e handler de verificação

--- 05-apis/primeira-api/main.go
+++ 05-apis/primeira-api/main.go
@@ -6,25 +6,38 @@ import (
 	"net/http"
 )
 
-// handler para a rota  /hello
+// handler para a rota /hello
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	// Só permitimos o método GET aqui
+	// só permitimos o método GET aqui
 	if r.Method != http.MethodGet {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		fmt.Fprintln(w, "Método não permetido")
+		w.WriteHeader(http.StatusMethodNotAllowed) // 405
+		fmt.Fprintln(w, "Método não permitido")
 		return
 	}
 
-	fmt.Fprintln(w, "Olá, Tata! Esta é a sua primeira API em Go")
+	fmt.Fprintln(w, "Olá, Tata! Esta é a sua primeira API em Go ")
+}
+
+// Handler para a rota /status
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintln(w, "Método não permitido")
+		return
+	}
+
+	fmt.Fprintln(w, "API rodando :) Tudo certo por aqui!!!")
 }
 
 func main() {
-	// registra s rota /hello e diz quem cuida dela é helloHandler
+	// Registra a rota /hello
 	http.HandleFunc("/hello", helloHandler)
 
-	fmt.Println("Servidor rodando em http://localhost:8080")
-	fmt.Println("Acesse http://localhost:8080/hello no navegador")
+	// Registra a rota /status
+	http.HandleFunc("/status", statusHandler)
 
-	// sobe servidor na porta 8080
+	fmt.Println("Servidor rodando em http://localhost:8080")
+	fmt.Println("Acesse http://localhost:8080/hello ou /status no navegador")
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
